0e96296f49325a273fc118d2bd6d72965a67d0b9
shameyang <shameyang@outlook.com>
2024-08-27 15:12:16 +0800

    Add: CorsConfig

---
+++ yangapi-backend/src/main/java/com/shameyang/yangapi/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.shameyang.yangapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ShameYang
+ * @date 2024/8/27 14:45
+ * @description 跨域配置
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 覆盖所有请求
+        registry.addMapping("/**")
+                // 允许发送 Cookie
+                .allowCredentials(true)
+                // 放行域名
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
+    }
+}
