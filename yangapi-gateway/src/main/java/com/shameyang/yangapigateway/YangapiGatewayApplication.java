904f325fd2b1c9b3ee76f8787721f98fb26ae50e
shameyang <shameyang@outlook.com>
2024-09-09 16:26:13 +0800

    Add: 添加 API 网关

---
+++ yangapi-gateway/src/main/java/com/shameyang/yangapigateway/YangapiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.shameyang.yangapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangapiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangapiGatewayApplication.class, args);
+    }
+}
