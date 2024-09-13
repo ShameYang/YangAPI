d025ee0b06c3bba3c89b6e09bcb9f5d45dc0d7cd
shameyang <shameyang@outlook.com>
2024-09-13 17:07:43 +0800

    fixed: 解决启动失败问题

--- yangapi-backend/src/main/java/com/shameyang/yangapi/YangapiBackendApplication.java
+++ yangapi-backend/src/main/java/com/shameyang/yangapi/YangapiBackendApplication.java
@@ -1,11 +1,14 @@
 package com.shameyang.yangapi;
 
+import com.shameyang.yangapiclientsdk.YangApiClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @MapperScan("com.shameyang.yangapi.mapper")
+@Import(YangApiClientConfig.class)
 public class YangapiBackendApplication {
 
 	public static void main(String[] args) {
